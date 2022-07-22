e4435ddd49612ed97fc2ead3f1b95f4e115a09e8
julinbestina <julin.bestina@yahoo.com>
2022-07-22 11:01:22 +0530

    Added code to inject Object dependancy using autowire byName

---
+++ SpringAutowiringQualifier/src/main/java/com/bl/autowired/Heart.java
@@ -0,0 +1,11 @@
+package com.bl.autowired;
+
+public class Heart {
+
+	public void pump() {
+		
+		System.out.println("Heart is pumping");
+		
+	}
+
+}
---
+++ SpringAutowiringQualifier/src/main/java/com/bl/autowired/SpringAutowiringQualifierApplication.java
@@ -0,0 +1,13 @@
+package com.bl.autowired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutowiringQualifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAutowiringQualifierApplication.class, args);
+	}
+
+}
