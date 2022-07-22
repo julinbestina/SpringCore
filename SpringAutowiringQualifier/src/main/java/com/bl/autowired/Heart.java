523a8f48901d3d5b00a32588c9b011ebbe4ff121
julinbestina <julin.bestina@yahoo.com>
2022-07-22 13:44:43 +0530

    Refactored the code to inject Object and literal dependancy using @Autowired and @Qualifier Annotation

--- SpringAutowiringQualifier/src/main/java/com/bl/autowired/Heart.java
+++ SpringAutowiringQualifier/src/main/java/com/bl/autowired/Heart.java
@@ -2,6 +2,25 @@ package com.bl.autowired;
 
 public class Heart {
 
+	private String name;
+	private int noOfHeart;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNoOfHeart() {
+		return noOfHeart;
+	}
+
+	public void setNoOfHeart(int noOfHeart) {
+		this.noOfHeart = noOfHeart;
+	}
+
 	public void pump() {
 
 		System.out.println("Heart is pumping");
--- SpringAutowiringQualifier/src/main/java/com/bl/autowired/Human.java
+++ SpringAutowiringQualifier/src/main/java/com/bl/autowired/Human.java
@@ -1,27 +1,22 @@
 package com.bl.autowired;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Human {
 
-	private Heart heart;
-
 	@Autowired
-	public Human(Heart heart) {
-
-		this.heart = heart;
-	}
-
-	public void setHeart(Heart heart) {
-
-		this.heart = heart;
-	}
+	@Qualifier("heartObject")
+	private Heart heart;
 
 	public void startPumping() {
 
 		if (heart != null) {
+			
 			heart.pump();
+			System.out.println("The name is " + heart.getName() + " and no. of Heart is " + heart.getNoOfHeart());
 		} else {
+			
 			System.out.println("Heart is not pumping");
 		}
 	}
